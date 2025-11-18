1517b3a2e3fdb7c16be7be39b786693144200252
abdihakimhersi <abdihakimhersi@gmail.com>
2025-11-18 21:52:07 +0000

    Enhance customer segmentation with geographical insights
    Added geographical insights and customer segmentation analysis based on total spent.

---
+++ sql/03_customer_segmentation.sql
@@ -0,0 +1,126 @@
+# Customer segmentation on total spent
+SELECT
+COUNT(*) AS number_of_customers,
+CASE WHEN total_spent < 305 THEN 'Low-Value'
+WHEN total_spent <=1654 THEN 'Medium-Value'
+ELSE 'High-Value' END AS customer_segment
+FROM(
+  SELECT
+    CustomerID,
+    SUM(total_order_value) AS total_spent,
+    AVG(total_order_value) AS average_order_value,
+    COUNT(DISTINCT(InvoiceNo)) AS number_of_purchases
+  FROM (
+    SELECT
+      CustomerID,
+      InvoiceNo,
+      SUM(UnitPrice*Quantity) AS total_order_value
+    FROM
+    `my-first-project-462019.online_retail.retail_data` 
+    WHERE
+    CHAR_LENGTH(InvoiceNo) = 6
+    AND CustomerID IS NOT NULL
+    GROUP BY
+      InvoiceNo,
+      CustomerID)
+  GROUP BY
+    CustomerID
+  ORDER BY
+    total_spent DESC)
+GROUP BY
+  customer_segment
+  
+# Geographical insights from segmentation 
+SELECT
+Country,
+COUNT(*) AS number_of_customers,
+CASE WHEN total_spent < 305 THEN 'Low-Value'
+WHEN total_spent <=1654 THEN 'Medium-Value'
+ELSE 'High-Value' END AS customer_segment
+FROM(
+  SELECT
+    CustomerID,
+    Country,
+    SUM(total_order_value) AS total_spent,
+    AVG(total_order_value) AS average_order_value,
+    COUNT(DISTINCT(InvoiceNo)) AS number_of_purchases
+  FROM (
+    SELECT
+      CustomerID,
+      InvoiceNo,
+      Country,
+      SUM(UnitPrice*Quantity) AS total_order_value
+    FROM
+    `my-first-project-462019.online_retail.retail_data` 
+    WHERE
+    CHAR_LENGTH(InvoiceNo) = 6
+    AND CustomerID IS NOT NULL
+    GROUP BY
+      InvoiceNo,
+      CustomerID,
+      Country)
+  GROUP BY
+    CustomerID,
+    Country
+  ORDER BY
+    total_spent DESC)
+GROUP BY
+  Country,
+  customer_segment
+ORDER BY
+  customer_segment,
+  Country
+  
+## Geographic Customer Segmentation Analysis
+SELECT
+*
+FROM(
+SELECT
+Country,
+customer_segment,
+number_of_customers,
+number_of_customers*100/SUM(number_of_customers)OVER(PARTITION BY Country) as segment_precentage,
+SUM(number_of_customers)OVER(PARTITION BY Country) AS total_country_customers
+FROM(
+SELECT
+Country,
+COUNT(*) AS number_of_customers,
+CASE WHEN total_spent < 305 THEN 'Low-Value'
+WHEN total_spent <=1654 THEN 'Medium-Value'
+ELSE 'High-Value' END AS customer_segment
+FROM(
+  SELECT
+    CustomerID,
+    Country,
+    SUM(total_order_value) AS total_spent,
+    AVG(total_order_value) AS average_order_value,
+    COUNT(DISTINCT(InvoiceNo)) AS number_of_purchases
+  FROM (
+    SELECT
+      CustomerID,
+      InvoiceNo,
+      Country,
+      SUM(UnitPrice*Quantity) AS total_order_value
+    FROM
+    `my-first-project-462019.online_retail.retail_data` 
+    WHERE
+    CHAR_LENGTH(InvoiceNo) = 6
+    AND CustomerID IS NOT NULL
+    GROUP BY
+      InvoiceNo,
+      CustomerID,
+      Country)
+  GROUP BY
+    CustomerID,
+    Country
+  ORDER BY
+    total_spent DESC)
+GROUP BY
+  Country,
+  customer_segment
+ORDER BY
+  customer_segment,
+  Country)
+  ORDER BY segment_precentage DESC)
+  WHERE total_country_customers > 10
+  ORDER BY segment_precentage DESC
