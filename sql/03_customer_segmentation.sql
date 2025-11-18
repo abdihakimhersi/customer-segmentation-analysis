dec459363ee65fe8f09f3ab57699d9b8c7ea916a
abdihakimhersi <abdihakimhersi@gmail.com>
2025-11-18 21:56:24 +0000

    Implement customer segmentation by total spent quartiles
    Added SQL query to calculate customer segmentation based on total spent quartiles.

--- sql/03_customer_segmentation.sql
+++ sql/03_customer_segmentation.sql
@@ -1,3 +1,30 @@
+# Tital spent quartiles
+SELECT
+  approx_quantiles(total_spent, 4) AS total_spent_quartiles
+FROM(
+  SELECT
+    CustomerID,
+    SUM(total_order_value) AS total_spent,
+    AVG(total_order_value) AS average_order_value,
+    COUNT(DISTINCT(InvoiceNo)) AS number_of_purchases
+  FROM (
+    SELECT
+      CustomerID,
+      InvoiceNo,
+      SUM(UnitPrice*Quantity) AS total_order_value
+    FROM
+    `my-first-project-462019.online_retail.retail_data` 
+    WHERE
+    CHAR_LENGTH(InvoiceNo) = 6
+    AND CustomerID IS NOT NULL
+    GROUP BY
+      InvoiceNo,
+      CustomerID)
+  GROUP BY
+    CustomerID
+  ORDER BY
+    total_spent DESC)
+  
 # Customer segmentation on total spent
 SELECT
 COUNT(*) AS number_of_customers,
