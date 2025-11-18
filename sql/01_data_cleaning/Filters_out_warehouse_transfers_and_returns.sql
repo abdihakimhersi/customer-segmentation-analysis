33ba72273025dfe324bc9d9627b951b61a795b93
abdihakimhersi <abdihakimhersi@gmail.com>
2025-11-18 20:53:23 +0000

    Enhance SQL script with NULL and invoice number analysis
    Added investigation queries for NULLs and invoice number differences.

--- sql/01_data_cleaning/Filters_out_warehouse_transfers_and_returns.sql
+++ sql/01_data_cleaning/Filters_out_warehouse_transfers_and_returns.sql
@@ -1,3 +1,4 @@
+# Investigating Nulls and differences in invoice numbers
 SELECT
   CustomerID,
   SUM(total_order_value) AS total_spent,
@@ -20,3 +21,14 @@ GROUP BY
   CustomerID
 ORDER BY
   total_spent DESC
+# Continuation of NULL investigation
+SELECT
+  COUNT(*) as number_of_invoices,
+  CHAR_LENGTH(InvoiceNo) as character_in_id
+FROM 
+  `my-first-project-462019.online_retail.retail_data`
+WHERE
+  CustomerID is NULL
+GROUP BY
+  character_in_id
+# key findings 6-character invoice is a legit customer 7-characters are warehouse transfer and NULL customerIds are returns
